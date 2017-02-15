a5fd5539b3481a1438f2ad91b6f68341204bbeb3
teyenliu <teyen.liu@gmail.com>
2017-02-15 15:49:02 +0800

    Add read mnist code

---
+++ read_mnist.py
@@ -0,0 +1,49 @@
+import os, struct, sys
+import numpy as np
+import pylab
+from array import array as pyarray
+from numpy import array, int8, uint8, zeros
+
+def load_mnist(dataset="training", digits=np.arange(10), path="."):
+    """
+    Loads MNIST files into 3D numpy arrays
+    """
+
+    if dataset == "training":
+        fname_img = os.path.join(path, 'train-images-idx3-ubyte')
+        fname_lbl = os.path.join(path, 'train-labels-idx1-ubyte')
+    elif dataset == "testing":
+        fname_img = os.path.join(path, 't10k-images-idx3-ubyte')
+        fname_lbl = os.path.join(path, 't10k-labels-idx1-ubyte')
+    else:
+        raise ValueError("dataset must be 'testing' or 'training'")
+
+    flbl = open(fname_lbl, 'rb')
+    magic_nr, size = struct.unpack(">II", flbl.read(8))
+    lbl = pyarray("b", flbl.read())
+    flbl.close()
+
+    fimg = open(fname_img, 'rb')
+    magic_nr, size, rows, cols = struct.unpack(">IIII", fimg.read(16))
+    img = pyarray("B", fimg.read())
+    fimg.close()
+
+    ind = [ k for k in range(size) if lbl[k] in digits ]
+    N = len(ind)
+
+    images = zeros((N, rows, cols), dtype=uint8)
+    labels = zeros((N, 1), dtype=int8)
+    for i in range(len(ind)):
+        images[i] = array(img[ ind[i]*rows*cols : (ind[i]+1)*rows*cols ]).reshape((rows, cols))
+        labels[i] = lbl[ind[i]]
+
+    return images, labels
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        images, labels = load_mnist('training', digits=[int(sys.argv[1])])
+    else:
+        images, labels = load_mnist('training')
+    pylab.imshow(images.mean(axis=0), cmap=pylab.cm.gray)
+    pylab.show()
