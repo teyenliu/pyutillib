07c0243b79cf8bb1e7a46dd75d40525ed1e04be0
teyenliu <teyen.liu@gmail.com>
2017-03-01 14:04:49 +0800

    Fix the way to use TPOTGegressor

--- wide_n_deep/tpot_yield_exp.py
+++ wide_n_deep/tpot_yield_exp.py
@@ -30,10 +30,44 @@ trX, teX, trY, teY = train_test_split(
     X, y, test_size=0.3, random_state=0)
     
 
-from tpot import TPOTClassifier
+from tpot import TPOTClassifier, TPOTRegressor
 # tpot 
-tpot = TPOTClassifier(generations=6, verbosity=2)
+tpot = TPOTRegressor(generations=10, verbosity=2)
 tpot.fit(trX, trY)
-tpot.score(teX, teY)
+print(tpot.score(teX, teY))
 # 导出
-tpot.export('pipeline.py')
+tpot.export('pipeline_yield.py')
+
+#================= use pipeline result ==========================
+from sklearn.ensemble import VotingClassifier
+from sklearn.linear_model import LassoLarsCV
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import Binarizer, FunctionTransformer, Normalizer
+from tpot.operators.preprocessors import ZeroCount
+
+exported_pipeline = make_pipeline(
+    ZeroCount(),
+    Binarizer(threshold=0.17),
+    Normalizer(norm="l1"),
+    LassoLarsCV(normalize=True)
+)
+
+exported_pipeline.fit(trX, trY)
+trY_pred = exported_pipeline.predict(trX)
+teY_pred = exported_pipeline.predict(teX)
+accuracy = exported_pipeline.score(teX, teY)
+
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+
+print('MSE train: %.3f, test: %.3f' % (
+        mean_squared_error(trY, trY_pred),
+        mean_squared_error(teY, teY_pred)))
+print('R^2 train: %.3f, test: %.3f' % (
+        r2_score(trY, trY_pred),
+        r2_score(teY, teY_pred)))
+
+"""
+R^2 train: 0.272, test: 0.193
+"""
