70181e95b3646df5024bbb62ef583d115eb48827
teyenliu <teyen.liu@gmail.com>
2018-07-06 16:15:15 +0800

    Add an example of confusion matrix

---
+++ mnist_gpu_confu_matrix.py
@@ -0,0 +1,177 @@
+# Common imports
+import sklearn as sk
+import tensorflow as tf
+from tensorflow.contrib import graph_editor as ge
+import numpy as np
+import os
+from tensorflow.examples.tutorials.mnist import input_data
+import memory_saving_gradients
+import mem_util
+#import linearize as linearize_lib
+
+# monkey patch tf.gradients to point to our custom version, with automatic checkpoint selection
+#tf.__dict__["gradients"] = memory_saving_gradients.gradients_memory
+
+
+mnist = input_data.read_data_sets("/home/liudanny/mnist_data/")
+
+#metrics
+def print_metrics(y_pred, y_batch):
+    print "Precision", sk.metrics.precision_score(y_batch, y_pred, labels=[0,1,2,3,4,5,6,7,8,9], average='weighted')
+    print "Recall", sk.metrics.recall_score(y_batch, y_pred, labels=[0,1,2,3,4,5,6,7,8,9], average='weighted')
+    print "f1_score", sk.metrics.f1_score(y_batch, y_pred, labels=[0,1,2,3,4,5,6,7,8,9], average='weighted')
+    print "confusion_matrix"
+    print sk.metrics.confusion_matrix(y_batch, y_pred, labels=[0,1,2,3,4,5,6,7,8,9])
+
+
+def get_model_params():
+    gvars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
+    return {gvar.op.name: value for gvar, value in zip(gvars, tf.get_default_session().run(gvars))}
+
+def restore_model_params(model_params):
+    gvar_names = list(model_params.keys())
+    assign_ops = {gvar_name: tf.get_default_graph().get_operation_by_name(gvar_name + "/Assign")
+                  for gvar_name in gvar_names}
+    init_values = {gvar_name: assign_op.inputs[1] for gvar_name, assign_op in assign_ops.items()}
+    feed_dict = {init_values[gvar_name]: model_params[gvar_name] for gvar_name in gvar_names}
+    tf.get_default_session().run(assign_ops, feed_dict=feed_dict)
+
+
+# to make this notebook's output stable across runs
+def reset_graph(seed=42):
+    tf.reset_default_graph()
+    tf.set_random_seed(seed)
+    np.random.seed(seed)
+
+height = 28
+width = 28
+channels = 1
+n_inputs = height * width
+
+conv1_fmaps = 32
+conv1_ksize = 3
+conv1_stride = 1
+conv1_pad = "SAME"
+
+conv2_fmaps = 64
+conv2_ksize = 3
+conv2_stride = 1
+conv2_pad = "SAME"
+conv2_dropout_rate = 0.25
+
+pool3_fmaps = conv2_fmaps
+
+n_fc1 = 128
+fc1_dropout_rate = 0.5
+
+n_outputs = 10
+
+reset_graph()
+
+with tf.device('/cpu:0'):
+    with tf.name_scope("inputs"):
+        X = tf.placeholder(tf.float32, shape=[None, n_inputs], name="X")
+        X_reshaped = tf.reshape(X, shape=[-1, height, width, channels])
+        y = tf.placeholder(tf.int32, shape=[None], name="y")
+        training = tf.placeholder_with_default(False, shape=[], name='training')
+    
+with tf.device('/gpu:0'):
+    conv1 = tf.layers.conv2d(X_reshaped, filters=conv1_fmaps, kernel_size=conv1_ksize,
+                             strides=conv1_stride, padding=conv1_pad,
+                             activation=tf.nn.relu, name="conv1")
+
+    conv2 = tf.layers.conv2d(conv1, filters=conv2_fmaps, kernel_size=conv2_ksize,
+                             strides=conv2_stride, padding=conv2_pad,
+                             activation=tf.nn.relu, name="conv2")
+    
+    with tf.name_scope("pool3"):
+        pool3 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="VALID")
+        pool3_flat = tf.reshape(pool3, shape=[-1, pool3_fmaps * 14 * 14])
+        pool3_flat_drop = tf.layers.dropout(pool3_flat, conv2_dropout_rate, training=training)
+    
+    with tf.name_scope("fc1"):
+        fc1 = tf.layers.dense(pool3_flat_drop, n_fc1, activation=tf.nn.relu, name="fc1")
+        fc1_drop = tf.layers.dropout(fc1, fc1_dropout_rate, training=training)
+    
+    with tf.name_scope("output"):
+        logits = tf.layers.dense(fc1, n_outputs, name="output")
+        Y_proba = tf.nn.softmax(logits, name="Y_proba")
+    
+    with tf.name_scope("train"):
+        xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y)
+        loss = tf.reduce_mean(xentropy)
+        optimizer = tf.train.AdamOptimizer()
+        training_op = optimizer.minimize(loss)
+    
+with tf.device('/cpu:0'):
+    with tf.name_scope("eval"):
+        prediction = tf.argmax(logits, 1)
+        correct = tf.nn.in_top_k(logits, y, 1)
+        accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+
+with tf.name_scope("init_and_save"):
+    init = tf.global_variables_initializer()
+    saver = tf.train.Saver()
+
+n_epochs = 1000
+batch_size = 8196
+
+best_loss_val = np.infty
+check_interval = 500
+checks_since_last_progress = 0
+max_checks_without_progress = 20
+best_model_params = None 
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth=True
+
+
+with tf.Session(config=config) as sess:
+    init.run()
+    for epoch in range(n_epochs):
+        for iteration in range(mnist.train.num_examples // batch_size):
+            X_batch, y_batch = mnist.train.next_batch(batch_size)
+            #run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+            #run_metadata = tf.RunMetadata()
+            _, r_logits, r_y_probal, correct, prediction, accuracy = sess.run([training_op, logits, Y_proba, correct, prediction, accuracy], 
+                feed_dict={X: X_batch, y: y_batch, training: True})
+                     #options=run_options,
+                     #run_metadata=run_metadata)
+
+            #correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+            print("correct, prediction, y_batch, accuracy: {},{},{},{}".format(\
+                correct, prediction, y_batch, accuracy))
+
+            #metrics
+            print_metrics(prediction, y_batch)
+
+            if iteration % check_interval == 0:
+                loss_val = loss.eval(feed_dict={X: mnist.validation.images,
+                                                y: mnist.validation.labels})
+                if loss_val < best_loss_val:
+                    best_loss_val = loss_val
+                    checks_since_last_progress = 0
+                    best_model_params = get_model_params()
+                else:
+                    checks_since_last_progress += 1
+                #mem_use = mem_util.peak_memory(run_metadata)['/gpu:0']/1e6
+                #print("Memory used: %.2f MB "%(mem_use))
+                max_bytes_in_use = sess.run(memory_stats_ops.MaxBytesInUse())/1e6
+                print("Max Memory used: %.2f MB "%(max_bytes_in_use))
+             
+        acc_train = accuracy.eval(feed_dict={X: X_batch, y: y_batch})
+        acc_val = accuracy.eval(feed_dict={X: mnist.validation.images,
+                                           y: mnist.validation.labels})
+        print("Epoch {}, train accuracy: {:.4f}%, valid. accuracy: {:.4f}%, valid. best loss: {:.6f}".format(
+                  epoch, acc_train * 100, acc_val * 100, best_loss_val))
+        if checks_since_last_progress > max_checks_without_progress:
+            print("Early stopping!")
+            break
+
+    if best_model_params:
+        restore_model_params(best_model_params)
+    acc_test = accuracy.eval(feed_dict={X: mnist.test.images,
+                                        y: mnist.test.labels})
+    print("Final accuracy on test set:", acc_test)
+    save_path = saver.save(sess, "./my_mnist_model")
+
